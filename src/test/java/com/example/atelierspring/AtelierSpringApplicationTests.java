f850d816dd61c8e3e2e789722f491ee297b1b0ab
EvaDiop10 <nadiop37@gmail.com>
2022-05-19 10:57:29 +0000

    first commit

---
+++ src/test/java/com/example/atelierspring/AtelierSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atelierspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtelierSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
